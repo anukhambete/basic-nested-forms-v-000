4354f4306b3cb94b3524c2930548fb1b8cb60832
Anu Khambete <anukhambete@gmail.com>
2018-10-10 11:27:12 +0000

    automatically backed up by learn

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,8 +1,8 @@
 class PeopleController < ApplicationController
   def new
     @person = Person.new
-    @person.addresses.build(address_type: 'one')
-    @person.addresses.build(address_type: 'two')
+    @person.addresses.build(address_type: 'work')
+    @person.addresses.build(address_type: 'home')
   end
 
   def create
